f5fbe7adb216b238cba7e2dd5a694216718b8f31
antonshvec <shvecanton1987@gmail.com>
2022-09-06 21:26:56 +0200

    Create HomeWork3.java

---
+++ HomeWork3.java
@@ -0,0 +1,26 @@
+/**
+ * Java Basic.Home work#3
+ * 
+ * @author Shvets Anton
+ * @version 06.09.22
+ * 
+ */     
+
+
+
+class HomeWork3 {
+public static void main(String[] args) {
+int[] myArray = new int[5];
+myArray[0] = 1;
+myArray[1] = 2;
+myArray[2] = 3;
+myArray[3] = 4;
+myArray[4] = 5;
+System.out.printf("%s\n", myArray[0]);
+System.out.printf("%s\n", myArray[1]);
+System.out.printf("%s\n", myArray[2]);
+System.out.printf("%s\n", myArray[3]);
+System.out.printf("%s\n", myArray[4]);
+
+}
+}
