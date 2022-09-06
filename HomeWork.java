1e833cc61f95e76df7298f84b36eb091a8e7c97c
antonshvec <shvecanton1987@gmail.com>
2022-09-06 21:26:13 +0200

    Create HomeWork.java

---
+++ HomeWork.java
@@ -0,0 +1,20 @@
+/**
+ * Java Basic.Home work#1
+ * 
+ * @author Shvets Anton
+ * @version 06.09.22
+ * 
+ */     
+
+
+     class HomeWork {
+     public static void main(String[] args) {
+     int a = 10;
+     int b = 5;
+     int p = (a + b)*2;
+     int s = a * b;
+     System.out.printf("Периметр %s\n", p);
+     System.out.printf("Площадь %s\n", s);
+     }
+     }
+     
