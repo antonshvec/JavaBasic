a1903b428f671203c43d16989c42a21c68025adb
antonshvec <shvecanton1987@gmail.com>
2022-09-06 21:26:59 +0200

    Create HomeWork4.java

---
+++ HomeWork4.java
@@ -0,0 +1,20 @@
+/**
+ * Java Basic.Home work#4
+ * 
+ * @author Shvets Anton
+ * @version 06.09.22
+ * 
+ */     
+
+
+
+class HomeWork4 {
+    public static void main(String[] args) {
+		int diametr = 10;
+		int kal1 = 40;
+		double plosh = diametr * Math.PI;
+		double kaloriynost = plosh * kal1;
+		System.out.printf("Всего каллорий %S",kaloriynost);
+	}
+}
+
