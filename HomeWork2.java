4f9edef1538965f887934539d30f83939f3f3223
antonshvec <shvecanton1987@gmail.com>
2022-09-06 21:26:54 +0200

    Create HomeWork2.java

---
+++ HomeWork2.java
@@ -0,0 +1,24 @@
+  /**
+ * Java Basic.Home work#2
+ * 
+ * @author Shvets Anton
+ * @version 06.09.22
+ * 
+ */     
+
+   
+     
+     class HomeWork2 {
+     public static void main(String[] args) {
+     int a = 5;
+     int b = 3;
+     int c = a + b;
+     int d = a - b;
+     int e = a * b;
+     double  i = (a * 0.1) / (b * 0.1);
+     System.out.printf("Сложение %s\n", c);
+     System.out.printf("Вычитание %s\n", d);
+     System.out.printf("Умножение %s\n", e);
+     System.out.printf("Деление %s\n", i);
+     }
+     }  
