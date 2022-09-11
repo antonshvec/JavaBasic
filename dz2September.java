735fcde6fe2639719b6a6f7c5c80cafbd7afa940
antonshvec <shvecanton1987@gmail.com>
2022-09-11 13:07:29 +0200

    Update dz2September.java

--- dz2September.java
+++ dz2September.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *  Java Basic. Homework #2
  * 
  * @author Shvets Anton
  * @todo 07.09.22
@@ -8,37 +8,34 @@
  
 class dz2September {
     public static void main(String[] args) {
-        checkWeather();
-        canWalk();
-        methodSlojeniya(9,5);
-        methodVichitaniya(9,5);
-        methodUmnojeniya(9,5);
+        checkWeather(0);
+        canWalk(true,false);
+        int a = methodSlojeniya(5,3);
+        int b =  methodVichitaniya(9,5);
+        int c = methodUmnojeniya(9,5);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         methodDeleniya(5,3);
     }
     
         //task 1
         
-    static String checkWeather() {
-        int temperature = 25;
+    static String checkWeather(int temperature) {
         if (temperature  <= 0  ) {
-           System.out.println("It’s frost");
+        System.out.println("It’s frost");
         } else if (temperature <=18 ) {
-           System.out.println("It’s cold");
+        System.out.println("It’s cold");
         } else if (temperature <=28 ) {
-           System.out.println("It’s warm");
+        System.out.println("It’s warm");
         } else if (temperature >=29 ) {
-           System.out.println("It’s hot");
-        }
-          else {
-           System.out.println("");
+        System.out.println("It’s hot");
         }
         return "";
-}
+    }
         //Task2
         
-    static boolean canWalk() {
-        boolean isWeekend = true;
-        boolean isRain = false;
+    static boolean canWalk(boolean isWeekend,boolean isRain) {
         if (isWeekend && !isRain) {
         System.out.println("Можно гулять");
         } else {
@@ -51,27 +48,23 @@ class dz2September {
         
         static int methodSlojeniya(int a, int b) {
         int c = a + b;
-        System.out.println(c);
         return c;
     }
         static int methodVichitaniya(int a, int b) {
         int c = a - b;
-        System.out.println(c);
         return c;
     
 }
         static int methodUmnojeniya(int a, int b) {
         int c = a * b;
-        System.out.println(c);
         return c;
 }
         static int methodDeleniya(int a, int b) {
         int c = a / b;
-        System.out.println((double) a / b);
+        System.out.println((double)a / b);
         return c;
     }
 }
-
 
 
 
